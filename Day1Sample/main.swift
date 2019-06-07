750274f3208443505485a82c275e4ba845c3adb1
hargunmarya <hargunmarya2726@gmail.com>
2019-06-06 21:02:49 -0400

    initial commit

--- Day1Sample/main.swift
+++ Day1Sample/main.swift
@@ -10,3 +10,32 @@ import Foundation
 
 print("Hello, World!")
 
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+let 😛="hello"
+
+print(😛,π)
+print(Int.max,Int.min)
+let a,b:Int
+var c:Int
+
+a=10
+b=3
+c=a+b
+print("addition is \(c)")
+
+c=a-b
+print(c)
+
+c=a*b
+print(c)
+
+c=a/b
+print(c)
+
+c=a%b
+print(c)
+
+
+
